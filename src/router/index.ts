db13827689705074d8b29c71bbf85e93e915f5da
Hoto Ras <hotoras03@gmail.com>
2025-01-25 17:47:34 +0900

    Update VueRouter

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import HomeView from '@/views/HomeView.vue';
 import TermsOfService from '@/views/TermsOfService.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const router = createRouter({
     history: createWebHashHistory(import.meta.env.BASE_URL),
